a730543a8b85bbd0ffaaebec6a01348c8b75410a
John Blackburn <JHBlackburn@gmail.com>
2026-05-19 13:23:57 -0400

    Enlarge mobile Troy cutout

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router";
 
 export default function HeroImage() {
   return (
-    <div className="relative flex h-full min-h-0 w-[132vw] max-w-none -translate-x-[8vw] items-end justify-center sm:w-[116vw] sm:-translate-x-[3vw] lg:w-full lg:max-w-[min(48vw,820px)] lg:translate-x-0 lg:justify-end">
+    <div className="relative flex h-full min-h-0 w-[165vw] max-w-none -translate-x-[20vw] items-end justify-center sm:w-[140vw] sm:-translate-x-[12vw] lg:w-full lg:max-w-[min(48vw,820px)] lg:translate-x-0 lg:justify-end">
       <div className="pointer-events-none absolute -bottom-8 right-[-10%] h-[80%] w-[84%] rounded-full bg-[#031226]/45 blur-3xl" />
 
       <img
         src="/troy-cutout.png"
         alt="Troy Albers"
-        className="pointer-events-none relative z-0 h-[min(58svh,34rem)] w-full max-w-none object-contain object-bottom drop-shadow-[0_1.4rem_1.8rem_rgba(0,0,0,0.42)] sm:h-[min(62svh,38rem)] lg:ml-auto lg:h-auto lg:max-h-[calc(100svh-8.1rem)] lg:w-auto lg:max-w-full xl:max-h-[calc(100svh-7.6rem)]"
+        className="pointer-events-none relative z-0 h-[min(68svh,42rem)] w-full max-w-none object-contain object-bottom drop-shadow-[0_1.4rem_1.8rem_rgba(0,0,0,0.42)] sm:h-[min(72svh,46rem)] lg:ml-auto lg:h-auto lg:max-h-[calc(100svh-8.1rem)] lg:w-auto lg:max-w-full xl:max-h-[calc(100svh-7.6rem)]"
       />
 
       <div className="absolute bottom-[clamp(1.1rem,6vh,4rem)] right-[clamp(0.2rem,4vw,3rem)] z-20 grid w-[min(82vw,30rem)] grid-cols-2 gap-[clamp(0.45rem,1.6vw,1rem)] lg:bottom-[clamp(2rem,9vh,6rem)] lg:right-[clamp(0rem,2vw,2rem)]">
